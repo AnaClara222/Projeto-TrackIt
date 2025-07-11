3fe6ba52b3ea92a6b77a0d3f67ae30f0e638bd2c
AnaClara222 <claritafonseca203@gmail.com>
2025-07-11 08:13:41 -0300

    Code adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { FooterContainer } from '../styles/FooterStyle';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 
-import { styled } from "styled-components";
 
 export default function Footer({ activeButton, onNavigateToday, onNavigateHabits }) {
-  const todayProgress = 60; 
 
   const habitsIsActive = activeButton === "habits";
   const todayIsActive = activeButton === "today"; 
@@ -49,14 +44,3 @@ export default function Footer({ activeButton, onNavigateToday, onNavigateHabits
   );
 }
 
-const CircularProgressBarContainer = styled.div`
-  width: 90px;
-  height: 90px;
-  margin-bottom: 30px;
-  position: relative;
-  z-index: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
